08c653368493aed5bf3d3c3c7ad4233531baff39
__coma__ <codeliner96@gmail.com>
2023-03-19 17:40:07 +0900

    With animation. (2)

--- components/responsive-horizontal.component.tsx
+++ components/responsive-horizontal.component.tsx
@@ -8,7 +8,7 @@ export default function ResponsiveHorizontal({
   children,
 }: ResponsiveHorizontalProps) {
   return (
-    <div className="w-full h-full text-xs sm:w-9/12 sm:text-sm md:w-6/12 md:text-md lg:w-5/12 lg:text-lg p-10 flex flex-col items-center gap-5">
+    <div className="w-full h-full text-xs sm:w-9/12 sm:text-sm md:w-6/12 md:text-md lg:w-5/12 lg:text-lg p-10 flex flex-col items-center">
       {children}
     </div>
   );
